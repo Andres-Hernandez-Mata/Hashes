530d0ec5d4eb2e604c46f825e2b701720787a2e5
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-24 09:56:12 -0500

    Hash MD5
    Lista de algoritmos disponibles

--- src/hashes.py
+++ src/hashes.py
@@ -6,3 +6,27 @@ Python: 3.9.1
 Fecha: 24 Abril 2020
 """
 
+import hashlib
+import time
+from datetime import datetime
+import os
+
+os.system('cls')
+
+now = datetime.now()
+
+print(now, '[INFO] Lista de algoritmos disponibles')
+time.sleep(1)
+print(hashlib.algorithms_available)
+
+texto = 'Hola'
+
+print(now, '[INFO] Texto')
+time.sleep(1)
+print(texto)
+
+md5_txt = hashlib.md5(texto.encode())
+print(now, '[INFO] Hash MD5 del texto')
+time.sleep(1)
+print(md5_txt.hexdigest())
+
