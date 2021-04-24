8bb4f8f153de7c88796127e6e234e20047b46df2
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-24 10:02:09 -0500

    Hash SHA-1
    Colores

--- src/hashes.py
+++ src/hashes.py
@@ -15,18 +15,26 @@ os.system('cls')
 
 now = datetime.now()
 
-print(now, '[INFO] Lista de algoritmos disponibles')
+print(now, '\033[0;32m [INFO] Lista de algoritmos disponibles \033[0;0m')
 time.sleep(1)
 print(hashlib.algorithms_available)
 
 texto = 'Hola'
 
-print(now, '[INFO] Texto')
+print(now, '\033[0;32m [INFO] Texto \033[0;0m')
 time.sleep(1)
 print(texto)
 
 md5_txt = hashlib.md5(texto.encode())
-print(now, '[INFO] Hash MD5 del texto')
+print(now, '\033[0;32m [INFO] Hash MD5 del Texto \033[0;0m')
 time.sleep(1)
 print(md5_txt.hexdigest())
+
+sha1_txt = hashlib.sha1(texto.encode())
+print(now, '\033[0;32m [INFO] Hash SHA-1 del Texto \033[0;0m')
+time.sleep(1)
+print(sha1_txt.hexdigest())
+
+
+
 
